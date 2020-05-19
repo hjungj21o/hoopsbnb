31c3d63884c78fc877a84afb01f7edd9da3fd776
haejun <haejun.chung210@gmail.com>
2020-05-19 15:43:31 -0400

    Debugged hoopers dropdown

--- frontend/components/arenas/arena_show_booking_box.jsx
+++ frontend/components/arenas/arena_show_booking_box.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'react-dates/initialize';
 import { DateRangePicker } from 'react-dates';
-import './_datepicker.css';
+// import './_datepicker.css';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
 
